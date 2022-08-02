c026b4feec08cc085dda208f6298102aae9156c5
elizabethrani <elizabethrani.madanu@eagles.oc.edu>
2022-08-02 09:09:55 -0500

    worked on download survey reports

---
+++ src/views/survey/DownloadSurveyReport.vue
@@ -0,0 +1,18 @@
+<template>
+  <download-csv :data="exportFileData" name="survey-report" class="">
+    <v-btn tile color="indigo" style="margin-top: 10px"
+      >DOWNLOAD SURVEY REPORT <v-icon right>mdi-download</v-icon>
+    </v-btn>
+  </download-csv>
+</template>
+<script>
+import JsonCSV from "vue-json-csv";
+export default {
+  components: {
+    downloadCsv: JsonCSV,
+  },
+  props: {
+    exportFileData: Array,
+  },
+};
+</script>
